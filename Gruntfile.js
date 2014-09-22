9c5bcf75041d3ecd3940cc5f0d6e086e955a1d3b
Emil van Galen <emil.van.galen@gmail.com>
2014-09-22 22:52:53 +0200

    Basic project skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = function(grunt) {
+    //grunt plugins
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+                }
+            }
+        },
+        karma: {
+            options: {
+                singleRun: true,
+                reporters: ['dots', 'coverage'],
+                preprocessors: {
+                    'src/**/*.js': ['coverage']
+                }
+            }
+        },
+        jshint: {
+            files: ['*.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                jshintrc: true
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'karma']
+        },
+        build: {
+
+        }
+    });
+
+    grunt.registerTask('test', ['jshint', 'karma']);
+
+    grunt.registerTask('default', ['jshint', 'karma', 'uglify']);
+};
